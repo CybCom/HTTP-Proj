5f0b4a3df7f19d38bb9bbece8767dcf2d480e311
Schepinhan <649836160@qq.com>
2022-12-06 16:57:01 +0800

    写完 解析状态码，注：Etag未实现

--- server/src/utils/JsonReader/ServerJsonReader.java
+++ server/src/utils/JsonReader/ServerJsonReader.java
@@ -1,12 +1,15 @@
 package utils.JsonReader;
+import HttpServer.HttpServer;
 import HttpServer.Message.Request;
 import HttpServer.Message.Response;
 import cn.hutool.core.io.FileUtil;
 import com.alibaba.fastjson.JSON;
 import utils.JsonReader.JavaBean.ServerDataBean;
 import utils.JsonReader.JavaBean.ServerResourceBean;
+import utils.MonthToNum;
 
-import java.io.File;
+import java.io.*;
+import java.util.Map;
 import java.util.Objects;
 
 public class ServerJsonReader {
@@ -32,7 +35,7 @@ public class ServerJsonReader {
         Response response = new Response();
         response.setVersion(request.getVersion());
         response.setCode(judgeCode(request));
-        setOthersByCode(response);
+        setOthersByCode(response,request);
         return response;
     }
 
@@ -42,46 +45,105 @@ public class ServerJsonReader {
      */
     private String judgeCode(Request request) {
         //TODO
-        for(ServerDataBean list : resourceBean.getResourceList()){
-            if(Objects.equals(request.getUrl(), list.getUrl())){
-                if(Objects.equals(request.getMethod(), list.getAllow())){
-                    boolean contains = request.getHeader().containsKey("If-None-Match") ||
-                            request.getHeader().containsKey("If-Modified-Since");
-                    if(contains){//304
-                        //TODO client 缓存 304
+        try{
+            for(ServerDataBean list : resourceBean.getResourceList()){
+                if(Objects.equals(request.getUrl(), list.getUrl())){
+                    if(Objects.equals(request.getMethod(), list.getAllow())){
+                        boolean contains = request.getHeader().containsKey("If-None-Match") ||
+                                request.getHeader().containsKey("If-Modified-Since");
+                        if(contains){//304
+                            if(request.getHeader().containsKey("If-None-Match")){//TODO maybe change
+                                for(ServerDataBean findEtag : resourceBean.getResourceList()){
+                                    if(Objects.equals(request.getHeader().get("If-None-Match"),
+                                            findEtag.getModifiedJudge().getEtag())){//TODO 弱比较方法
+                                        return "304";
+                                    }
+                                }
+                                return "200";
+                            }
+                            if(request.getHeader().containsKey("If-Modified-Since")){
+                                String[] sModified = list.getModifiedJudge().getLast_modified().split(" ");
+                                String[] rModified = request.getHeader().get("Last-Modified").split(" ");
+                                String sourceModified = sModified[3]+ MonthToNum.Month_Map.get(sModified[2])
+                                        + sModified[1]+ sModified[4];
+                                String requestModified = rModified[3]+ MonthToNum.Month_Map.get(rModified[2])
+                                        + rModified[1]+ rModified[4];
+                                if(sourceModified.compareTo(requestModified) <= 0){//远端资源的 Last-Modified 首部标识的日期比在该首部中列出的值要更早，
+                                                                                   // 条件匹配不成功
+                                    return "304";
+                                }else {
+                                    return "200";//TODO maybe change
+                                }
+                            }
+                        }
+                        boolean isMove = list.getReLocationJudge().getIsMove();
+                        if(!isMove) {//TODO Maybe Wrong
+                            return "200";
+                        }
+                        boolean isPermanent = list.getReLocationJudge().getIsPermanent();
+                        if(isPermanent){
+                            return "301";
+                        }
+                        return "302";
                     }
-                    boolean isMove = list.getReLocationJudge().getIsMove();
-                    if(isMove) return "200";
-                    boolean isPermanent = list.getReLocationJudge().getIsPermanent();
-                    if(isPermanent) return "301";
-                    return "302";
+                    return "405";
                 }
-                return "405";
             }
+            return "404";
+        }catch (Exception e){
+            return "500";
         }
-        return "404";
+
     }
 
-    private void setOthersByCode(Response response) {
+    /**
+     * @param response 回复报文
+     * 返回根据状态码改变的报文
+     * */
+    private void setOthersByCode(Response response, Request request) {
         //TODO
         switch (response.getCode()) {
-            case 200:
-                break;
-            case 301:
-                break;
-            case 302:
-                break;
-            case 304:
-                break;
-            case 404:
-                break;
-            case 405:
-                break;
-            case 500:
-                break;
-            default:
-                break;
+            case 200 -> {          //头部有 Cache-Control, Content-Location, Date, ETag, Expires，和 Vary.
+                response.setStatus("OK");
+                response.setMessage(ReturnMessage(request.getUrl()));
+            }
+            case 301 -> {
+                response.setStatus("Moved Permanently");
+                for (ServerDataBean list : resourceBean.getResourceList()) {
+                    if (Objects.equals(list.getUrl(), request.getUrl())) {
+                        response.getHeader().put("Location", list.getReLocationJudge().getNew_url());
+                    }
+                }
+                response.setMessage(ReturnMessage(request.getUrl()));
+            }
+            case 302 -> {
+                response.setStatus("Found");
+                response.setMessage(ReturnMessage(request.getUrl()));
+            }
+            case 304 -> response.setStatus("Not Modified");
+            case 404 -> response.setStatus("Not Found");
+            case 405 -> response.setStatus("Method Not Allowed");
+            case 500 -> response.setStatus("Internal Server Error");
+            default -> {
+            }
         }
+    }
+
+    private String ReturnMessage(String url){//TODO
+        InputStream resource = HttpServer.class.getResourceAsStream(HttpServer.ROOT_PATH + url);
+        StringBuilder sb = new StringBuilder();
+        try{
+            assert resource != null;
+            BufferedReader bf = new BufferedReader(new InputStreamReader(resource));
+            String line;
+            while ((line = bf.readLine()) != null) {
+                sb.append(line);
+                sb.append("\r\n");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
